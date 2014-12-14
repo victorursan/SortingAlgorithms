e97b5ed27bfaca2acab04f82a08cb886e307cc43
Victor Ursan <victor.ursan@gmail.com>
2014-12-14 17:39:07 +0200

    implemented ShakeSort/CocktailSort

--- src/sorting_algorithms/algorithms/ShakeSort.py
+++ src/sorting_algorithms/algorithms/ShakeSort.py
@@ -11,4 +11,13 @@ class ShakeSort(GenericSort):
     self.col[:] = self.__shake_sort(self.col)
 
   def __shake_sort(self, l):
-    return l
+    up = range(len(l) - 1)
+    while True:
+      for indices in (up, reversed(up)):
+        swapped = False
+        for i in indices:
+          if not self._in_order(l[i], l[i + 1]):
+            l[i], l[i + 1] = l[i + 1], l[i]
+            swapped = True
+        if not swapped:
+          return l
